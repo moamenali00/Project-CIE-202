70d22e3d7b5c02a5592abdea9c43815f8cdacb7d
moamenali00 <s-moamen.ali@zewailcity.edu.eg>
2023-01-06 00:04:31 +0200

    m

--- Shapes/Rpoly.cpp
+++ Shapes/Rpoly.cpp
@@ -252,5 +252,28 @@ shape* Rpoly::duplicate(GUI* pGUI) {
 }
 
 void Rpoly::Hide(GUI* pGUI) {
+    ShpGfxInfo.isHidden = true;
+    int min_x = x[0];
+    int min_y = y[0];
+    int max_x = x[0];
+    int max_y = y[0];
+    for (int i = 1; i < vertcies; i++)
+    {
+        if (x[i] < min_x)
+            min_x = x[i];
 
+        if (x[i] > max_x)
+            max_x = x[i];
+    }
+    for (int i = 1; i < vertcies; i++)
+    {
+        if (y[i] < min_y)
+            min_y = y[i];
+
+        if (y[i] > max_y)
+            max_y = y[i];
+    }
+    int rad = sqrt(((min_x - max_x) * (min_x - max_x)) + ((min_y - max_y) * (min_y - max_y)));
+    string hide = "images\\Hide.jpg";
+    pGUI->draw_image(hide, min_x, min_y, rad, rad);
 }
--- Shapes/Square.cpp
+++ Shapes/Square.cpp
@@ -174,5 +174,10 @@ shape* Square::duplicate(GUI* pGUI) {
 }
 
 void Square::Hide(GUI* pGUI) {
-
+	ShpGfxInfo.isHidden = true;
+	int rad = sqrt(((Corner1.x - Corner2.x) * (Corner1.x - Corner2.x)) + ((Corner1.y - Corner2.y) * (Corner1.y - Corner2.y)));
+	string hide = "images\\Hide.jpg";
+	int min_x = min(Corner1.x, Corner2.x);
+	int min_y = min(Corner1.y, Corner2.y);
+	pGUI->draw_image(hide, min_x, min_y, rad, rad);
 }
